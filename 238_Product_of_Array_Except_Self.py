c7b9393f79923d860e62ce58168d128ebd0a4f15
hieuminh65 <hieuminhnguyen@usf.edu>
2023-08-10 13:36:59 -0400

    Update more Problems

--- 238_Product_of_Array_Except_Self.py
+++ 238_Product_of_Array_Except_Self.py
@@ -1,18 +1,26 @@
-def productExceptSelf(nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        result = []
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        if nums.count(0) > 1:
+            return [0] * len(nums)
+        if 0 in nums:
+            product = 1
+            inOf0 = nums.index(0)
+            for i in range(len(nums)):
+                if i == inOf0:
+                    continue
+                product *= nums[i]
+                nums[i] = 0
+            nums[inOf0] = product
+            return nums
+        product = 1
+
         for i in range(len(nums)):
-            
+
+            product *= nums[i]
+
+        for i in range(len(nums)):
+            nums[i] = product // nums[i]
+        return nums
 
 
 
-            
-        
-
-        return result
-
-re = productExceptSelf([1,2,3,4])
-print(re) #[24,12,8,6]
